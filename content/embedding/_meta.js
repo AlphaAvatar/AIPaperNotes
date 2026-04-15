e3702588f53fddc04266123ad8a924ccf1272e1c
Licheng Wang <244267620@qq.com>
2026-04-15 15:18:14 +0800

    update

---
+++ content/embedding/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  index: 'Overview',
+  'text-embedding': 'Text Embedding',
+}
---
+++ content/visual-encoder/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  index: 'Overview',
+  'image-segment-pretraining': 'Image Segment Pretraining',
+  'language-image-pretraining': 'Language Image Pretraining',
+}
